fb2c5840e89ffdeb11a13189fb1446ae657e53ae
SJChoudhury-au28 <93276880+SJChoudhury-au28@users.noreply.github.com>
2021-12-15 08:38:34 +0530

    basic python codes

---
+++ code/evenoddcount.py
@@ -0,0 +1,17 @@
+# to count Even
+# and Odd numbers in a List
+
+# list of numbers
+list1 = [1,2,3,4,5,6,7,8,9,10]
+even_count, odd_count = 0, 0
+
+for num in list1:
+
+    if num % 2 == 0:
+        even_count += 1
+
+    else:
+        odd_count += 1
+
+print("Even numbers in the list: ", even_count)
+print("Odd numbers in the list: ", odd_count)
