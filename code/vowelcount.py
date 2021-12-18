851202dfac3485a600906ffe003823b5d9310a4f
SJChoudhury-au28 <93276880+SJChoudhury-au28@users.noreply.github.com>
2021-12-18 17:45:22 +0530

    basic codes of python

---
+++ code/vowelcount.py
@@ -0,0 +1,9 @@
+
+def Check_Vow(string, vowels):
+    final = [each for each in string if each in vowels]
+    print(final)
+
+# Driver Code
+string = input("Enter any sentence ")
+vowels = "AaEeIiOoUu"
+Check_Vow(string, vowels)
